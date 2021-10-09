72a3bb159ab46756850892ace6798ad8fe75cd40
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-10-10 01:15:36 +0530

    Initial Commit

---
+++ src/main/java/io/yadnyesh/springbootunittestrahulshetty/SpringbootunittestrahulshettyApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.springbootunittestrahulshetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootunittestrahulshettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootunittestrahulshettyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yadnyesh/springbootunittestrahulshetty/SpringbootunittestrahulshettyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.springbootunittestrahulshetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootunittestrahulshettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
