ee25b1c9840bb2d5b0985a69c7ce17dd5598a4fb
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-12-25 18:40:45 +0530

    Building POST mapping

---
+++ src/main/java/io/yadnyesh/springbootunittestrahulshetty/AddBookResponse.java
@@ -0,0 +1,15 @@
+package io.yadnyesh.springbootunittestrahulshetty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddBookResponse {
+    private String msg;
+    private String id;
+}
--- src/main/java/io/yadnyesh/springbootunittestrahulshetty/controller/LibraryController.java
+++ src/main/java/io/yadnyesh/springbootunittestrahulshetty/controller/LibraryController.java
@@ -1,8 +1,11 @@
 package io.yadnyesh.springbootunittestrahulshetty.controller;
 
+import io.yadnyesh.springbootunittestrahulshetty.AddBookResponse;
 import io.yadnyesh.springbootunittestrahulshetty.model.Library;
 import io.yadnyesh.springbootunittestrahulshetty.repository.LibraryRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +21,12 @@ public class LibraryController {
     }
 
     @PostMapping("/books")
-    public void addBookToLibrary(@RequestBody Library library) {
+    public ResponseEntity<AddBookResponse> addBookToLibrary(@RequestBody Library library) {
         library.setId(library.getIsbn()+library.getAisle());
-        log.info(library.toString());
         libraryRepository.save(library);
+        AddBookResponse addBookResponse = new AddBookResponse();
+        addBookResponse.setMsg("The book is successfully added to library collection");
+        addBookResponse.setId(library.getIsbn()+library.getAisle());
+        return new ResponseEntity<>(addBookResponse, HttpStatus.OK);
     }
 }
