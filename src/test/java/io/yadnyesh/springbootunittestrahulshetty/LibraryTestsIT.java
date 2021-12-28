b7e754a35e6fb6b4d3aa9628ab0f77c28defd7e8
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-12-28 10:35:49 +0530

    Integration Tests with H2 DB - POST endpoint

--- src/test/java/io/yadnyesh/springbootunittestrahulshetty/LibraryTestsIT.java
+++ src/test/java/io/yadnyesh/springbootunittestrahulshetty/LibraryTestsIT.java
@@ -1,12 +1,14 @@
 package io.yadnyesh.springbootunittestrahulshetty;
 
+import io.yadnyesh.springbootunittestrahulshetty.model.Library;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 @SpringBootTest
 @Slf4j
@@ -20,5 +22,26 @@ public class LibraryTestsIT {
         log.info(responseEntity.getStatusCode().toString());
         log.info(responseEntity.getBody());
         JSONAssert.assertEquals(expectedJSON, responseEntity.getBody(),false);
+    }
+
+    @Test
+    public void addBookToLibraryTest() {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity request = new HttpEntity<Library>(buildLibrary(), httpHeaders);
+        ResponseEntity<String> response = testRestTemplate.postForEntity("http://localhost:8080/books",request, String.class);
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        log.info(response.getHeaders().get("unique").toString());
+    }
+
+    public Library buildLibrary() {
+        Library library = new Library();
+        library.setAisle(100);
+        library.setBook_name("Spring");
+        library.setIsbn("book");
+        library.setId("book100");
+        library.setAuthor("yadnyesh");
+        return library;
     }
 }
